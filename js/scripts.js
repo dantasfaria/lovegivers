6213cd749f23c8dc3e71ea0b42c92043378dcf54
dantasfaria <gabrieldantasfgs@gmail.com>
2022-09-05 23:31:04 -0300

    implementa auto scroll off

--- js/scripts.js
+++ js/scripts.js
@@ -47,3 +47,24 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+const myCarouselElement = document.querySelector('#demo')
+const myCarouselElement2 = document.querySelector('#demo2')
+const myCarouselElement3 = document.querySelector('#demo3')
+const myCarouselElement4 = document.querySelector('#demo4')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: false,
+})
+
+const carousel2 = new bootstrap.Carousel(myCarouselElement2, {
+    interval: false,
+  })
+
+const carousel3 = new bootstrap.Carousel(myCarouselElement3, {
+  interval: false,
+})
+
+const carousel4 = new bootstrap.Carousel(myCarouselElement4, {
+    interval: false,
+  })
